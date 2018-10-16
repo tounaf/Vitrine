35dcbb4e4519c5a36227f6f78b10a29058cac0a2
hery <rasolonjatovohery0011@gmail.com>
2018-10-16 23:11:01 +0200

    ajout de contenue

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
 
-  root to: 'home#index'
+  root to: 'home#index' , as: :presentation
 
-  get '/mission' , to: 'home#mission' , as: :'mission_path'
+  get '/mission' , to: 'home#mission' , as: :mission
 
-  get '/realisation' , to: 'home#realisation' , as: :'realisation_path'
+  get '/realisation' , to: 'home#realisation' , as: :realisation
 
-  get 'partenaire' , to: 'home#partenaire', as: :'partenaire_path'
+  get '/partenaire' , to: 'home#partenaire', as: :partenaire
 
-  get 'garanties' , to: 'home#garanties' , as: :'garenties_path'
+  get '/garanties' , to: 'home#garanties' , as: :garanties
 
-  get 'contact' , to: 'home#contact' , as: :'contact_path'
+  get '/contact' , to: 'home#contact' , as: :contact
 end
